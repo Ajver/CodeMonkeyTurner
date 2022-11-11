2d1c1c703bdad8bf965f7f5ba12e655ce3297da3
Dominik <zawlockidominik@gmail.com>
2022-11-11 16:51:25 +0100

    Mouse raycasting to world plane

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -18,11 +18,6 @@ public class Unit : MonoBehaviour
             float moveSpeed = 4f;
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
         }
-        
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            Move(new Vector3(4, 0, 4));
-        }
     }
     
     private void Move(Vector3 targetPosition)
