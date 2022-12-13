9b38139296a251b512b7660d8bcb29b8d684ab0a
Dominik <zawlockidominik@gmail.com>
2022-12-13 20:45:52 +0100

    Update healthBar in Selected Unit UI

--- Assets/Scripts/UnitRelated/SelectedUnitUI.cs
+++ Assets/Scripts/UnitRelated/SelectedUnitUI.cs
@@ -1,19 +1,25 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SelectedUnitUI : MonoBehaviour
 {
+
+    [SerializeField] private Image healthBar;
     
     void Start()
     {
         UnitActionSystem.Instance.OnSelectedUnitChanged += UnitActionSystem_OnSelectedUnitChanged;
+        Unit.OnAnyUnitDamaged += Unit_OnAnyUnitDamaged;
         
         UpdateVisibility();
+        UpdateHealthBar();
     }
 
     private void UnitActionSystem_OnSelectedUnitChanged(object sender, EventArgs e)
     {
         UpdateVisibility();
+        UpdateHealthBar();
     }
 
     private void UpdateVisibility()
@@ -24,6 +30,23 @@ public class SelectedUnitUI : MonoBehaviour
         gameObject.SetActive(shouldBeVisible);
     }
 
+    private void Unit_OnAnyUnitDamaged(object sender, EventArgs e)
+    {
+        UpdateHealthBar();
+    }
+    
+    private void UpdateHealthBar()
+    {
+        Unit unit = UnitActionSystem.Instance.GetSelectedUnit();
+
+        if (unit == null)
+        {
+            return;
+        }
+
+        healthBar.fillAmount = unit.GetHealthNormalized();
+    }
+    
     public void OnFocusBtnClicked()
     {
         UnitActionSystem.Instance.FocusOnUnit();
@@ -33,10 +56,14 @@ public class SelectedUnitUI : MonoBehaviour
     {
         UnitActionSystem.Instance.ChangeSelectedUnit(-1);
     }
-    
+
     public void OnNextUnitBtnClicked()
     {
         UnitActionSystem.Instance.ChangeSelectedUnit(+1);
     }
-    
+
+    private void OnDestroy()
+    {
+        Unit.OnAnyUnitDamaged -= Unit_OnAnyUnitDamaged;
+    }
 }
